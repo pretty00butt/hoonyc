9960670411f9f572211de19866723a14a2cf0c27
the6thm0nth <the6thm0nth@outlook.com>
2018-08-03 12:52:53 +0900

    Update: use github password when not to store but deploy dynamically

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -1,4 +1,6 @@
 import { Command, flags } from "@oclif/command"
+import co from "co"
+import { password } from "co-prompt"
 
 import { load } from "../lib/config"
 import deploy from "../lib/deploy"
@@ -16,18 +18,22 @@ export default class Deploy extends Command {
 
   async run() {
     const { flags } = this.parse(Deploy)
+    const log = this.log
 
     const { app = "", env = "" } = flags
     const _config = load()
     const config = _config[app][env]
-    this.log(env)
-    this.log(_config[app])
 
-    deploy({
-      app,
-      env,
-      ...config,
-      log: this.log
+    co(function*() {
+      const githubPassword = yield password("Github Password: ")
+
+      deploy({
+        app,
+        env,
+        log,
+        githubPassword,
+        ...config
+      })
     })
   }
 }
